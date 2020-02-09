48e3c5643f96ab39a6263ad05303b4cda9952269
Raphael <justdevops@gmail.com>
2020-02-09 11:53:09 +0200

    Arie WebApp

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello Tikal, this is my simple Flask Webpage"
+    return "Hello Arie, this is my simple Flask Webpage"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=8080)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
 import app
 
 def test_app():
-    assert app.main() == "Hello Tikal, this is my simple Flask Webpage"
+    assert app.main() == "Hello Arie, this is my simple Flask Webpage"
